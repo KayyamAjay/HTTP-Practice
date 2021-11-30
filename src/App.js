795be4586f7d95e164c7e38ba9da7e116808228d
Ajay Kayyam <saiajay220@gmail.com>
2021-11-30 15:55:44 +0530

    Adding ErrorState

--- src/App.js
+++ src/App.js
@@ -6,20 +6,32 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
   async function fetchHandler() {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films/"); //fetch functions is automatically set to get
-    const data = await response.json();
-    const transformedMovies = data.results.map((moviedata) => {
-      return {
-        id: moviedata.episode_id,
-        title: moviedata.title,
-        openingText: moviedata.opening_crawl,
-        releaseDate: moviedata.release_date,
-      };
-    });
-    setMovies(transformedMovies);
-    setIsLoading(false);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/film/"); //fetch functions is automatically set to get
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const data = await response.json();
+      const transformedMovies = data.results.map((moviedata) => {
+        return {
+          id: moviedata.episode_id,
+          title: moviedata.title,
+          openingText: moviedata.opening_crawl,
+          releaseDate: moviedata.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+      setIsLoading(false);
+    }
   }
 
   return (
@@ -29,8 +41,9 @@ function App() {
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>No Movies Found</p>}
+        {!isLoading && movies.length === 0 && !error && <p>No Movies Found</p>}
         {isLoading && <p>Loading....</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
